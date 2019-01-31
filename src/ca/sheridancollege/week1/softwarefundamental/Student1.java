5358f643abb13c028655dbf40cbd4ae8275d1221
SharonPatel <47011170+SharonPatel@users.noreply.github.com>
2019-01-31 15:34:20 -0500

    copyright date added

--- src/ca/sheridancollege/week1/softwarefundamental/Student1.java
+++ src/ca/sheridancollege/week1/softwarefundamental/Student1.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Date 31st Jan 2019.
  */
 package ca.sheridancollege.week1.softwarefundamental;
 
